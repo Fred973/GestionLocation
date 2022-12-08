b2ff2544ac70096626c54cf3883f12462d0b90cf
fred <helicesky@gmail.com>
2022-12-08 10:00:38 +0100

    update

--- soft/constant.py
+++ soft/constant.py
@@ -1,10 +1,10 @@
 import os
 
 # Constant configuration variable for Flask
-user = 'root'
+user = 'fred'
 mdp = 'Fred973*'
 mysql_sqalchemy = 'mysql+pymysql://{}:{}@localhost/gestion_loc'.format(user, mdp)
-secret_key = "ma superbe cle secrete"
+secret_key = "9d7ac8ce47a5159ef96bd29f316cad4a"
 local_host = 'localhost'
 db_name = 'gestion_loc'
 
