9abc2d426fd68d35ae674f1cdd891555042f7182
fred <helicesky@gmail.com>
2022-12-08 10:48:47 +0100

    update

--- reset_table_DB.py
+++ reset_table_DB.py
@@ -6,12 +6,12 @@ with app.app_context():
 
     db.create_all()
 
-user = Users(
+    user = Users(
     id=1,
     username='fred',
     password_hash='sha256$uvPHv3uhXGtYRt0h$5090871eb4c28b38128af516a3d5c1a4611a81babf273529076e5e1aadc7dc6b'
 
-)
-db.session.add(user)
-db.session.commit()
+    )
+    db.session.add(user)
+    db.session.commit()
 
